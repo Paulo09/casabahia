bddafdaf0197c1e887e7468462a3d94610579e6e
Paulo09 <paullocasttro@hotmail.com>
2024-06-26 17:42:08 -0300

    Prova Casaas Bahia

---
+++ src/test/java/br/com/casasbahia/exemplo/vendedores/ExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casasbahia.exemplo.vendedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
